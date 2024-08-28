d779807511acf864cb0b0fa008f4d981dcd02f5e
estreat1ng <matiarietto@hotmail.com>
2024-08-27 23:20:34 -0300

    "Agregado js, falta css"

--- app.js
+++ app.js
@@ -1,39 +1,65 @@
-//  vars
-const inputText = document.getElementById('input');
-const outputText = document.getElementById('output');
-const notFoundMessage = document.getElementById('not-found-msg');
+const textArea = document.querySelector(".content__input");
+const mensaje = document.querySelector(".texto-modificado");
+const copyButton = document.querySelector('.copiar');
 
-//  buttons
-const encriptarButton = document.getElementById('encriptar__btn');
-const desencriptarButton = document.getElementById('desencriptar__btn');
-const copiarButton = document.getElementById('copiar__btn');
+/*    
+La letra "e" es convertida para "enter"
+La letra "i" es convertida para "imes"
+La letra "a" es convertida para "ai"
+La letra "o" es convertida para "ober"
+La letra "u" es convertida para "ufat"
+*/
 
-//  agregar la funcion a los buttons
-encriptarButton.addEventListener('click', encriptarTexto(inputText.value));
-desencriptarButton.addEventListener('click', desencriptarTexto(inputText.value));
-
-//  desactivar output hasta que sea necesario (o activar)
-outputText.style.display = "none";
-copiarButton.style.display = "none";
-
-
-function encriptarTexto(texto) {
-    //  llaves de encriptacion
-    /*const letraE = inputText.replace("e", "enter");
-    const letraI = inputText.replace("i", "imes");
-    const letraA = inputText.replace("a", "ai");
-    const letraO = inputText.replace("o", "ober");
-    const letraU = inputText.replace("u", "ufat");*/
-
+function botonEncriptar(){ 
+    const textoEncriptado = encriptar(textArea.value);
+    mensaje.value = textoEncriptado;
+    textArea.value = "";
+    mensaje.style.backgroundImage = "none";
 }
 
-function encriptarTexto(texto) {
-    //  llaves de encriptacion
-    const letraE = outputText.replace("enter", "e");
-    const letraI = outputText.replace("imes", "i");
-    const letraA = outputText.replace("ai", "a");
-    const letraO = outputText.replace("ober", "o");
-    const letraU = outputText.replace("ufat", "u");
-    
-    
+function encriptar(stringEncriptada) {
+    let matrizCodigo = [["e", "enter"], ["i", "imes"], ["a", "ai"], ["o", "ober"], ["u", "ufat"]];
+    stringEncriptada = stringEncriptada.toLowerCase();
+
+    for (let i = 0; i < matrizCodigo.length; i++) {
+        stringEncriptada = stringEncriptada.replaceAll(matrizCodigo[i][0], matrizCodigo[i][1]);
     }
+    return stringEncriptada;
+}
+
+function botonDesencriptar(){ 
+    const textoDesencriptado = desencriptar(textArea.value);
+    mensaje.value = textoDesencriptado;
+    textArea.value = "";
+}
+
+function desencriptar(stringDesencriptada) {
+    let matrizCodigo = [["enter", "e"], ["imes", "i"], ["ai", "a"], ["ober", "o"], ["ufat", "u"]];
+    stringDesencriptada = stringDesencriptada.toLowerCase();
+
+    for (let i = 0; i < matrizCodigo.length; i++) {
+        stringDesencriptada = stringDesencriptada.replaceAll(matrizCodigo[i][0], matrizCodigo[i][1]);
+    }
+    return stringDesencriptada;
+}
+
+function copiarAlPortapapeles() {
+    mensaje.select(); 
+    mensaje.setSelectionRange(0, 99999);
+
+    navigator.clipboard.writeText(mensaje.value)
+        .then(() => {
+            alert('Texto copiado con éxito');
+        })
+        .catch(err => {
+            alert('Error al copiar el texto: ' + err);
+        });
+}
+function mostrarMensajeExito() {
+    const mensajeExito = document.getElementById('mensaje-exito');
+    mensajeExito.style.display = 'block';
+    setTimeout(() => {
+        mensajeExito.style.display = 'none';
+    }, 2000);
+}
+
